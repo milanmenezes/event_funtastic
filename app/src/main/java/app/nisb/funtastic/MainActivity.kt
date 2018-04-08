cc3049e8474d54036d1e6cd6b50fd38b8f472784
milanmenezes <milanmenezes@gmail.com>
2018-04-08 23:19:53 +0530

    Ankura Treasure Hunt by Computer Society

--- app/src/main/java/app/nisb/funtastic/MainActivity.kt
+++ app/src/main/java/app/nisb/funtastic/MainActivity.kt
@@ -21,13 +21,16 @@ import com.android.volley.toolbox.Volley
 
 
 
-var code = arrayListOf<String>("a9b7ba70783b617e9998dc4dd82eb3c5",
-    "b8c37e33defde51cf91e1e03e51657da",
-    "fba9d88164f3e2d9109ee770223212a0",
-    "aa68c75c4a77c87f97fb686b2f068676",
-    "fed33392d3a48aa149a87a38b875ba4a",
-    "2387337ba1e0b0249ba90f55b2ba2521",
-    "5f268dfb0fbef44de0f668a022707b86")
+var code = arrayListOf<String>("38b3eff8baf56627478ec76a704e9b52",
+    "ec8956637a99787bd197eacd77acce5e",
+    "6974ce5ac660610b44d9b9fed0ff9548",
+    "c9e1074f5b3f9fc8ea15d152add07294",
+    "65b9eea6e1cc6bb9f0cd2a47751a186f",
+    "f0935e4cd5920aa6c7c996a5ee53a70f",
+    "a97da629b098b75c294dffdc3e463904",
+        "a3c65c2974270fd093ee8a9bf8ae7d0b",
+        "2723d092b63885e0d7c260cc007e8b9d",
+        "5f93f983524def3dca464469d2cf9f3e")
 var curr = 0
 var cur1= 0
 
@@ -54,6 +57,9 @@ class MainActivity : AppCompatActivity() {
             q5.visibility = View.GONE
             q6.visibility = View.GONE
             q7.visibility = View.GONE
+            q8.visibility = View.GONE
+            q9.visibility = View.GONE
+            q10.visibility = View.GONE
 
 
 
@@ -86,8 +92,20 @@ class MainActivity : AppCompatActivity() {
                 q6.visibility = View.VISIBLE
             }
 
-            if (test >= 7) {
-                q7.visibility = View.VISIBLE
+        if (test >= 7) {
+            q7.visibility = View.VISIBLE
+        }
+
+        if (test >= 8) {
+            q8.visibility = View.VISIBLE
+        }
+
+        if (test >= 9) {
+            q9.visibility = View.VISIBLE
+        }
+
+            if (test >= 10) {
+                q10.visibility = View.VISIBLE
                 longToast("Congratulations!!")
             }
 
@@ -95,62 +113,81 @@ class MainActivity : AppCompatActivity() {
 
         if (group==0) {
 
-            code = arrayListOf<String>("a9b7ba70783b617e9998dc4dd82eb3c5",
-                    "b8c37e33defde51cf91e1e03e51657da",
-                    "fba9d88164f3e2d9109ee770223212a0",
-                    "aa68c75c4a77c87f97fb686b2f068676",
-                    "fed33392d3a48aa149a87a38b875ba4a",
-                    "2387337ba1e0b0249ba90f55b2ba2521",
-                    "5f268dfb0fbef44de0f668a022707b86")
+            code = arrayListOf<String>("38b3eff8baf56627478ec76a704e9b52",
+                    "ec8956637a99787bd197eacd77acce5e",
+                    "6974ce5ac660610b44d9b9fed0ff9548",
+                    "c9e1074f5b3f9fc8ea15d152add07294",
+                    "65b9eea6e1cc6bb9f0cd2a47751a186f",
+                    "f0935e4cd5920aa6c7c996a5ee53a70f",
+                    "a97da629b098b75c294dffdc3e463904",
+                    "a3c65c2974270fd093ee8a9bf8ae7d0b",
+                    "2723d092b63885e0d7c260cc007e8b9d",
+                    "5f93f983524def3dca464469d2cf9f3e")
 
                 q0.text = "1) Start from Notice Board!"
-                q1.text = "2) Keeping food cold is important you see,\n This is the place where the meal may be."
-                q2.text = "3) I am the source of knowledge, found in every knowledge. Even though I vanished,\n but present in your college."
-                q3.text = "4) When light goes, E=PZΦn/A come to rescue."
-                q4.text = "5) Some like me hot, some like me cold,\n some like me mild but some like me bold."
-                q5.text = "6) My birthday is (celebrated as) your day.\n You drink water because of me. Find me in college."
-                q6.text = "7) The wheel is come full circle,\n the end of the journey is the beginning of our run."
-                q7.text = "Congratulations!!\n You have Completed the Treasure Hunt!!"
+                q1.text = "2) Gourmet in an instant. Every moment has its flavor"
+                q2.text = "3) A place of all love, with balls from above, \nthe way you must get, is split with a net."
+                q3.text = "4) I have no doors or windows, but still stand \nhere with my twin to keep you free from  Hunger"
+                q4.text = "5) Gotta keep things green and fresh. \nWhen I’m working, I’ll make your seats wet"
+                q5.text = "6) I come to the rescue when you are in trouble every month, \nI have things more than worth, I provide you with all your needs \nbut you gotta walk through a subterrain in return of all  my deeds."
+                q6.text = "7) Beside the place of fun and game, I stand big with a greater fame, \nNot many of you have stepped inside it, \nas it takes a test of your technical wit."
+                q7.text = "8) Find the shoe to continue the clue."
+                q8.text = "9) KA09 is my family name, Try finding my name  110101111000"
+                q9.text = "10) Three boys stand in a row, on the face of a door uphold,\n First two being the young twins, the third one is definitely elder and bold. \n Add them up and get a 7 nod, let me tell you all these three brothers are odd."
+                q10.text = "Congratulations!!\n You have Completed the Treasure Hunt!!"
             }
 
         if (group==1) {
 
-            code = arrayListOf<String>("a9b7ba70783b617e9998dc4dd82eb3c5",
-                    "d0fb963ff976f9c37fc81fe03c21ea7b",
-                    "4ba29b9f9e5732ed33761840f4ba6c53",
-                    "2387337ba1e0b0249ba90f55b2ba2521",
-                    "b8b4b727d6f5d1b61fff7be687f7970f",
-                    "d47268e9db2e9aa3827bba3afb7ff94a",
-                    "5f268dfb0fbef44de0f668a022707b86")
+            code = arrayListOf<String>("38b3eff8baf56627478ec76a704e9b52",
+                    "854d6fae5ee42911677c739ee1734486",
+                    "e2c0be24560d78c5e599c2a9c9d0bbd2",
+                    "274ad4786c3abca69fa097b85867d9a4",
+                    "eae27d77ca20db309e056e3d2dcd7d69",
+                    "7eabe3a1649ffa2b3ff8c02ebfd5659f",
+                    "69adc1e107f7f7d035d7baf04342e1ca",
+                    "091d584fced301b442654dd8c23b3fc9",
+                    "b1d10e7bafa4421218a51b1e1f1b0ba2",
+                    "5f93f983524def3dca464469d2cf9f3e")
 
             q0.text = "1) Start from Notice Board!"
-            q1.text = "2) Its a place of DJ where you go to\n Dance and play, wanna go out take a U-Turn."
-            q2.text = "3) I love my girls, you are so sweet,\n This comfy place is where princesses sleep."
-            q3.text = "4) My birthday is (celebrated as) your day.\n You drink water because of me. Find me in college."
-            q4.text = "5) To solve the hunt , you must get into the room wired with net, crambed with tech and full of code with the clue hidden in this building."
-            q5.text = "6) KA 09 ___________ \nI'm the third son in the family.Me, my twin, an elder brother who is twice my age and sister a year younger are standing in an order of birth. sum of our age is 19. Find the blank."
-            q6.text = "7) The wheel is come full circle, the end of the journey is the beginning of our run."
-            q7.text = "Congratulations!!\n You have Completed the Treasure Hunt!!"
+            q1.text = "2) I have buttons but I’m not a shirt, I have doors but I’m not a house\nI go up and down but I’m not an umbrella,\nI need at least two stories but I’m not a book of fairytales, I’m found in tall buildings but I’m not a penthouse"
+            q2.text = "3) It’s been a long day, I’m about to burst, rush to me to quench your thirst"
+            q3.text = "4) You use me and yet I don’t complain, a seeking green puppet under the blue sky who holds your mess"
+            q4.text = "5) There's a place your teachers meet,  a portal place where people greet,  also keep lost and found,  find this area with a fine toothed comb"
+            q5.text = "6) An elevated place with substance and a story where many masquerades with their hidden talents carve a path to their glory"
+            q6.text = "7) I have a net but do not catch, you go up and down but I stand still watching the match."
+            q7.text = "8) I’m a landmark,  I’m a mystery who keeps all your secrets safe. Saving you from the sunburn, wanna go somewhere? Take a n U-turn."
+            q8.text = "9) Known for shatarupa, the one praised by the wise, she resides in our college in the form of ___"
+            q9.text = "10) Three boys stand in a row, on the face of a door uphold, First two being the young twins, the third one is definitely elder and bold.  Add them up and get a 7 nod, let me tell you all these three brothers are odd."
+            q10.text = "Congratulations!!\n You have Completed the Treasure Hunt!!"
         }
 
         if (group==2) {
 
-            code = arrayListOf<String>("a9b7ba70783b617e9998dc4dd82eb3c5",
-                    "908c9a564a86426585b29f5335b619bc",
-                    "2387337ba1e0b0249ba90f55b2ba2521",
-                    "a4380923dd651c195b1631af7c829187",
-                    "20479c788fb27378c2c99eadcf207e7f",
-                    "3a61ed715ee66c48bacf237fa7bb5289",
-                    "5f268dfb0fbef44de0f668a022707b86")
+            code = arrayListOf<String>("38b3eff8baf56627478ec76a704e9b52",
+                    "577bcc914f9e55d5e4e4f82f9f00e7d4",
+                    "11b9842e0a271ff252c1903e7132cd68",
+                    "37bc2f75bf1bcfe8450a1a41c200364c",
+                    "496e05e1aea0a9c4655800e8a7b9ea28",
+                    "b2eb7349035754953b57a32e2841bda5",
+                    "8e98d81f8217304975ccb23337bb5761",
+                    "a8c88a0055f636e4a163a5e3d16adab7",
+                    "eddea82ad2755b24c4e168c5fc2ebd40",
+                    "5f93f983524def3dca464469d2cf9f3e")
 
             q0.text = "1) Start from Notice Board!"
-            q1.text = "2) You pass through me everyday with all the respect, Reminds your identity when he is there for inspect."
-            q2.text = "3) My birthday is (celebrated as) your day. You drink water because of me. Find me in college."
-            q3.text = "4) Caution is needed going this way, A tarmac place of bay bay bay. Now is the time to leave the car, If you leave here you'll not go far."
-            q4.text = "5) I have a pouch but i cant catch fish. I see shots fired and all your colour full sneakers. Find me you seeker."
-            q5.text = "6) You gulp it down or sip it slow, this is the place where you forget to frown."
-            q6.text = "7) The wheel is come full circle, the end of the journey is the beginning of our run."
-            q7.text = "Congratulations!!\n You have Completed the Treasure Hunt!!"
+            q1.text = "2) I'm full of water, but don't take a sip. I'm for feathered friends who want a dip."
+            q2.text = "3) Seek not any higher, I hide behind tyre, Now is the time to leave the car\n" +
+                    "If you leave from here you’ll go far"
+            q3.text = "4) I’m a 7 lettered word, the first 3 letters make you drink from me and the other 4 is a phase of life. The clue is easy to crack, when you are hungry and all dark"
+            q4.text = "5) I’m the biggest, I’m the gloriest. I treasure all the Musical nights and dance fights."
+            q5.text = "6) Oh how you boys wish to take a look inside, but here I sit in my small room pushing you all aside."
+            q6.text = "7) An open air place for your athletic skills to show, where we all sweat but never frown, look beneath you, isn't the colour just too brown?"
+            q7.text = "8) Dare you come near me, I can burn you blight. I’m the supreme power who keeps your room bright."
+            q8.text = "9) You pass through me, I welcome you all. Don’t be late, or else you have to climb the wall."
+            q9.text = "10) Three boys stand in a row, on the face of a door uphold,\n First two being the young twins, the third one is definitely elder and bold. \n Add them up and get a 7 nod, let me tell you all these three brothers are odd."
+            q10.text = "Congratulations!!\n You have Completed the Treasure Hunt!!"
         }
 
 
@@ -180,11 +217,11 @@ class MainActivity : AppCompatActivity() {
 
                     if (num==code[curr]){
 
-                        if (curr<6){
+                        if (curr<9){
                         curr+=1
                         }
 
-                        if (cur1<7)
+                        if (cur1<10)
                             cur1+=1
 
                         val sharedpreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE)
@@ -228,8 +265,17 @@ class MainActivity : AppCompatActivity() {
                         if (num==code[5])
                             q6.visibility = View.VISIBLE
 
-                        if (num==code[6]) {
+                        if (num==code[6])
                             q7.visibility = View.VISIBLE
+
+                        if (num==code[7])
+                            q8.visibility = View.VISIBLE
+
+                        if (num==code[8])
+                            q9.visibility = View.VISIBLE
+
+                        if (num==code[9]) {
+                            q10.visibility = View.VISIBLE
                             longToast("Congratulations!!")
                         }
 
